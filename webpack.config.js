72d19e1bdc6988cedf37e756b64576eb88447b95
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-07-02 18:33:33 +0100

    uploaded images and styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js'
+    mode: 'development',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", 
+                    "css-loader",
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, "public")
+        },
+    
+        compress: true,
+        port: 3010, // default 8000
+      },
 }
