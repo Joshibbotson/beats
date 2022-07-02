8fe336ee1e23ce6b4ca4fb7d563ede2f366e9925
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-07-02 22:54:28 +0100

    further progress on home page

--- src/home.js
+++ src/home.js
@@ -3,12 +3,10 @@ function home() {
     const nav = document.createElement('nav')
     const header = document.createElement('header')
     const main = document.createElement('main')
-    const div = document.createElement('div')
-    const li = document.createElement('li')
     const ul = document.createElement('ul')
     const h2 = document.createElement('h2')
-    const liHTML = document.getElementsByTagName('li')
 
+    //nav//
     const h2Text = document.createTextNode('Myhzy Beats')
     h2.classList.add('nav-title')
     h2.appendChild(h2Text)
@@ -34,8 +32,45 @@ function home() {
     ul.appendChild(homeLi)
     ul.appendChild(beatsLi)
     ul.appendChild(contactLi)
-    
+// nav ends//
+//header//
+    content.appendChild(header)
 
+    const profileImage = document.createElement('div')
+    const textContainer = document.createElement('div')
+    const h2Element = document.createElement('h1')
+    const pElement = document.createElement('p')
+
+    const headerH2Text = document.createTextNode("Hip Hopper, Go Getter")
+    const headerPText = document.createTextNode("Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id non molestiae temporibus molestias laudantium, dignissimos dicta officia ex minus culpa, expedita, architecto ratione provident cum! Sapiente, perferendis quibusdam tenetur eveniet repudiandae beatae omnis veritatis porro consequatur rem explicabo qui corporis exercitationem! Voluptate, suscipit architecto temporibus doloremque in optio quas facilis!")
+
+    h2Element.appendChild(headerH2Text)
+    h2Element.classList.add('h1-header')
+    pElement.appendChild(headerPText)
+
+    textContainer.classList.add('text-container')
+    textContainer.append(h2Element)
+    textContainer.append(pElement)
+
+    header.appendChild(profileImage)
+    profileImage.classList.add('profile-image')
+
+    header.appendChild(textContainer)
+    //header ends
+    //main..
+    content.appendChild(main)
+
+    function itemCreation (amount, cssClass) {
+        let arr = []
+        for (let i = 0; i <= amount; i++) {
+            const div = document.createElement('div')
+            div.classList.add(cssClass)
+            arr.push(div)
+            main.appendChild(arr[i])
+        }
+        console.log(arr)
+    }
+    itemCreation(3, "item")
 
 
 }
